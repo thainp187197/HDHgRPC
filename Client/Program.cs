0295c9dcccd89bfc907b71048119cf1f8dc7be26
NGO PHU THAI 20187197 <thai.np187197@sis.hust.edu.vn>
2022-01-18 11:08:22 +0700

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Net.Client;
+using Server;
+using System;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var greeterClient = new Greeter.GreeterClient(channel);
+            var greeterReply = await greeterClient.SayHelloAsync(new HelloRequest { Name = "Myself" });
+            Console.WriteLine("Greeting: " + greeterReply.Message);
+
+            var calculatorClient = new Calculator.CalculatorClient(channel);
+            var calculatorRequest = new IntFactorRequest() { X = 10, Y = 5 };
+            var addResponse = await calculatorClient.AddAsync(calculatorRequest);
+            var subtractResponse = await calculatorClient.SubtractAsync(calculatorRequest);
+            var multiplyResponse = await calculatorClient.MultiplyAsync(calculatorRequest);
+            var divideResponse = await calculatorClient.DivideAsync(calculatorRequest);
+            Console.WriteLine($"Calculator:\n" +
+                $"Addition: {calculatorRequest.X} + {calculatorRequest.Y} = {addResponse.F}\n" +
+                $"Subtraction: {calculatorRequest.X} - {calculatorRequest.Y} = {subtractResponse.F}\n" +
+                $"Multiplication: {calculatorRequest.X} * {calculatorRequest.Y} = {multiplyResponse.F}\n" +
+                $"Divisor: {calculatorRequest.X} / {calculatorRequest.Y} = {divideResponse.F}\n");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Server/Services/CalculatorService.cs
@@ -0,0 +1,45 @@
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class CalculatorService : Calculator.CalculatorBase
+    {
+        public override Task<ResultReply> Add(IntFactorRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ResultReply
+            {
+                F = request.X + request.Y,
+            });
+        }
+
+        public override Task<ResultReply> Subtract(IntFactorRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ResultReply
+            {
+                F = request.X - request.Y,
+            });
+        }
+
+        public override Task<ResultReply> Multiply(IntFactorRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ResultReply
+            {
+                F = request.X * request.Y,
+            });
+        }
+
+        public override Task<ResultReply> Divide(IntFactorRequest request, ServerCallContext context)
+        {
+            if (request.Y == 0) throw new ArgumentException("Divisor must not be zero.");
+            return Task.FromResult(new ResultReply
+            {
+                F = request.X / request.Y,
+            });
+        }
+    }
+}
